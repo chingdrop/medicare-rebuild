07ab8c475cfabaf7862d99e02493a3e76ba5667b
Craig Hurley <churley@metflservices.com>
2025-01-21 12:44:55 -0500

    query to reset tables

---
+++ reset_tables.sql
@@ -0,0 +1,12 @@
+﻿DELETE FROM patient_address;
+DBCC CHECKIDENT ('patient_address', RESEED, 0);
+
+DELETE FROM patient_insurance;
+DBCC CHECKIDENT ('patient_insurance', RESEED, 0);
+
+DELETE FROM medical_necessity;
+DBCC CHECKIDENT ('medical_necessity', RESEED, 0);
+
+DELETE FROM patient
+	WHERE patient_id > (SELECT MIN(patient_id) FROM patient);
+DBCC CHECKIDENT ('patient', RESEED, 1);
