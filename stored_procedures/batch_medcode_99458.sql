ecf2388f31097c4656b824a54e9520dd80b5f0ab
Craig Hurley <churley@metflservices.com>
2025-01-23 18:50:18 -0500

    add all sprocs from sql server

---
+++ stored_procedures/batch_medcode_99458.sql
@@ -0,0 +1,71 @@
+﻿-- =============================================
+-- Author:		Craig Hurley
+-- Create date: 12/26/24
+-- Description:	Adds 99458 to medical code for patients with 20 mins of RPM.
+-- =============================================
+CREATE PROCEDURE [dbo].[batch_medcode_99458]	
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+
+	DROP TABLE IF EXISTS #99458
+	DROP TABLE IF EXISTS #numbers
+
+	CREATE TABLE #numbers (
+		n INT PRIMARY KEY
+	);
+
+	INSERT INTO #numbers (n)
+	VALUES (1), (2), (3), (4), (5), (6), (7), (8), (9), (10); 
+
+	WITH patient_rpm AS (
+		SELECT pn.patient_id,
+			FLOOR(SUM(pn.call_time_seconds) /1200) AS rpm_20_mins_blocks
+		FROM patient_note pn
+		WHERE pn.note_datetime >= DATEADD(MONTH, -1, GETDATE())
+		GROUP BY pn.patient_id
+	),
+	patient_code AS (
+		SELECT mc.patient_id,
+			COALESCE(COUNT(CASE WHEN mct.name = '99458' THEN mc.patient_id END), 0) AS code_count
+		FROM medical_code mc
+		LEFT JOIN medical_code_type mct
+			ON mc.med_code_type_id = mct.med_code_type_id
+		WHERE mc.timestamp_applied >= DATEADD(MONTH, -1, GETDATE())
+		GROUP BY mc.patient_id
+	)
+
+	SELECT pn.patient_id,
+		MAX(pn.note_datetime) AS latest_note
+	INTO #99458
+	FROM patient_note pn
+	JOIN patient_rpm pr
+		ON pn.patient_id = pr.patient_id
+	JOIN patient_code pc
+		ON pn.patient_id = pc.patient_id
+		AND pr.rpm_20_mins_blocks - pc.code_count > 0
+		AND pc.code_count < 3
+	JOIN #numbers n
+		ON n.n <= (pr.rpm_20_mins_blocks - pc.code_count - 1)
+	WHERE pn.patient_id IN (
+		SELECT mc.patient_id
+		FROM medical_code mc
+		JOIN medical_code_type mct 
+			ON mc.med_code_type_id = mct.med_code_type_id
+			AND mct.name = '99457'
+			AND mc.timestamp_applied >= DATEADD(MONTH, -1, GETDATE())
+	)
+	GROUP BY pn.patient_id
+
+	INSERT INTO medical_code (patient_id, med_code_type_id, timestamp_applied)
+	SELECT t.patient_id,
+		(
+		SELECT mct.med_code_type_id
+		FROM medical_code_type mct
+		WHERE mct.name = '99458'
+		),
+		t.latest_note
+	FROM #99458 t
+
+END
