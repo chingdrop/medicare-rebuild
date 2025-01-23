fdeaf39e6246dc97c0512fb01189369854636017
Craig Hurley <chingdorp@pm.me>
2025-01-23 18:50:18 -0500

    add all sprocs from sql server

---
+++ stored_procedures/batch_medcode_99454_bp.sql
@@ -0,0 +1,56 @@
+﻿-- =============================================
+-- Author:		Craig Hurley
+-- Create date: 12/23/24
+-- Description:	Adds 99454 code to medical code for patients with blood pressure monitors.
+-- =============================================
+CREATE PROCEDURE batch_medcode_99454_bp
+
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+	DROP TABLE IF EXISTS #99454_bp
+
+	SELECT bpr.patient_id,
+		MAX(bpr.received_datetime) AS latest_reading
+	INTO #99454_bp
+	FROM blood_pressure_reading bpr
+	WHERE bpr.received_datetime >= DATEADD(day, -30, GETDATE())
+	AND bpr.patient_id IN (
+		SELECT mc.patient_id
+		FROM medical_code mc
+		JOIN medical_code_type mct 
+			ON mc.med_code_type_id = mct.med_code_type_id
+			AND mct.name = '99453'
+		JOIN medical_code_device mcd
+			ON mct.med_code_type_id = mcd.med_code_type_id
+		JOIN device d
+			ON mcd.device_id = d.device_id
+			AND d.name LIKE '%glucometer%'
+	) AND bpr.patient_id NOT IN (
+		SELECT mc.patient_id
+		FROM medical_code mc
+		JOIN medical_code_type mct 
+			ON mc.med_code_type_id = mct.med_code_type_id
+			AND mct.name = '99454'
+			AND mc.timestamp_applied >= DATEADD(day, -30, GETDATE())
+		JOIN medical_code_device mcd
+			ON mct.med_code_type_id = mcd.med_code_type_id
+		JOIN device d
+			ON mcd.device_id = d.device_id
+			AND d.name LIKE '%glucometer%'
+	)
+	GROUP BY bpr.patient_id
+		HAVING COUNT(DISTINCT CAST(bpr.received_datetime AS DATE)) = 16
+
+	INSERT INTO medical_code (patient_id, med_code_type_id, timestamp_applied)
+	SELECT t.patient_id,
+		(
+		SELECT mct.med_code_type_id
+		FROM medical_code_type mct
+		WHERE mct.name = '99454'
+		),
+		t.latest_reading
+	FROM #99454_bp t
+
+END
---
+++ stored_procedures/batch_medcode_99457.sql
@@ -0,0 +1,39 @@
+﻿-- =============================================
+-- Author:		Craig Hurley
+-- Create date: 12/23/24
+-- Description:	Adds 99457 to medical code for patients with 20 mins of RPM.
+-- =============================================
+CREATE PROCEDURE batch_medcode_99457 
+
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+	DROP TABLE IF EXISTS #99457
+
+    SELECT pn.patient_id,
+		MAX(pn.note_datetime) AS latest_note
+	INTO #99457
+	FROM patient_note pn
+	WHERE pn.note_datetime >= DATEADD(MONTH, -1, GETDATE())
+	AND pn.patient_id NOT IN (
+		SELECT mc.patient_id
+		FROM medical_code mc
+		JOIN medical_code_type mct 
+			ON mc.med_code_type_id = mct.med_code_type_id
+			AND mct.name = '99457'
+			AND mc.timestamp_applied >= DATEADD(MONTH, -1, GETDATE())
+	)
+	GROUP BY pn.patient_id
+		HAVING SUM(pn.call_time_seconds) / 60 >= 20
+
+	INSERT INTO medical_code (patient_id, med_code_type_id, timestamp_applied)
+	SELECT t.patient_id,
+		(
+		SELECT mct.med_code_type_id
+		FROM medical_code_type mct
+		WHERE mct.name = '99457'
+		),
+		t.latest_note
+	FROM #99457 t
+END
