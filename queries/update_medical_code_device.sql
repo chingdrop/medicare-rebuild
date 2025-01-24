d630c90525e2d67933ab68a8b98fdbd8fed8d8aa
Craig Hurley <churley@metflservices.com>
2025-01-24 17:47:32 -0500

    add rows to medical code device table

---
+++ queries/update_medical_code_device.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO medical_code_device(med_code_type_id, device_id)
+SELECT mct.med_code_type_id, d.device_id
+FROM medical_code_type mct
+CROSS JOIN device d
+WHERE mct.name in ('99453', '99454')
