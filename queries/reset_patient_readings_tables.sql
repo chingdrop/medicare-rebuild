4e51bde64a7bb7b2396c55bd5f7c88b1b7323cb3
Craig Hurley <churley@metflservices.com>
2025-01-24 15:28:43 -0500

    add device

--- queries/reset_patient_readings_tables.sql
+++ queries/reset_patient_readings_tables.sql
@@ -5,3 +5,7 @@ DBCC CHECKIDENT ('blood_pressure_reading', RESEED, 0);
 PRINT('Glucose Reading');
 DELETE FROM glucose_reading;
 DBCC CHECKIDENT ('glucose_reading', RESEED, 0);
+
+PRINT('Device');
+DELETE FROM device;
+DBCC CHECKIDENT ('device', RESEED, 0);
