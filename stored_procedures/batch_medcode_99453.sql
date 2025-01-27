677629ab577e5ab7316e03e70e41c52ad867cffb
Craig Hurley <churley@metflservices.com>
2025-01-27 15:31:41 -0500

    consolidate and update 99453

---
+++ stored_procedures/batch_medcode_99453.sql
@@ -0,0 +1,59 @@
+﻿-- =============================================
+-- Author:		Craig Hurley
+-- Create date: 12/20/24
+-- Revision date: 01/27/25
+-- Description:	Adds 99453 code to medical code for patients with 16 distinct days of testing.
+-- =============================================
+CREATE PROCEDURE batch_medcode_99453
+
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+	DROP TABLE IF EXISTS #99453;
+
+	DECLARE @inserted_codes TABLE (
+		med_code_id INT, patient_id INT
+	);
+
+	SELECT d.patient_id
+	INTO #99453
+	FROM device d
+	LEFT JOIN glucose_reading gr
+		ON d.device_id = gr.device_id
+	LEFT JOIN blood_pressure_reading bpr
+		ON d.device_id = bpr.device_id
+	WHERE NOT EXISTS (
+		SELECT 1
+		FROM medical_code mc
+		JOIN medical_code_type mct 
+			ON mc.med_code_type_id = mct.med_code_type_id
+			AND mct.name = '99453'
+		JOIN medical_code_device mcd
+			ON mc.med_code_id = mcd.med_code_id
+		WHERE mc.patient_id = d.patient_id
+			AND mcd.device_id = d.device_id
+	)
+	GROUP BY d.patient_id, d.device_id
+		HAVING COUNT(DISTINCT CAST(gr.received_datetime AS DATE)) >= 16
+			OR COUNT(DISTINCT CAST(bpr.received_datetime AS DATE)) >= 16;
+
+	INSERT INTO medical_code (patient_id, med_code_type_id, timestamp_applied)
+	OUTPUT INSERTED.med_code_id, INSERTED.patient_id 
+		INTO @inserted_codes (med_code_id, patient_id)
+	SELECT t.patient_id,
+		(
+		SELECT mct.med_code_type_id
+		FROM medical_code_type mct
+		WHERE mct.name = '99453'
+		),
+		GETDATE()
+	FROM #99453 t;
+	
+	INSERT INTO medical_code_device (med_code_id, device_id)
+	SELECT ic.med_code_id, d.device_id
+	FROM @inserted_codes ic
+	JOIN device d
+		ON ic.patient_id = d.patient_id;
+
+END
