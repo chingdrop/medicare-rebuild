d227714f13683c0764141eb1e1f27418e7caa6bf
Craig Hurley <churley@metflservices.com>
2025-01-30 13:00:57 -0500

    add device and medical code device

--- queries/reset_medical_code_tables.sql
+++ queries/reset_medical_code_tables.sql
@@ -1,3 +1,11 @@
+PRINT('Device');
+DELETE FROM device;
+DBCC CHECKIDENT ('device', RESEED, 0);
+
 PRINT('Medical Code');
 DELETE FROM medical_code;
 DBCC CHECKIDENT ('medical_code', RESEED, 0);
+
+PRINT('Medical Code Device');
+DELETE FROM medical_code_device;
+DBCC CHECKIDENT ('medical_code_device', RESEED, 0);
