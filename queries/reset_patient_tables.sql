58ebf9a0fa7e62d803af4dae96813f01c7bbdd55
Craig Hurley <churley@metflservices.com>
2025-01-30 13:00:45 -0500

    add patient status

--- queries/reset_patient_tables.sql
+++ queries/reset_patient_tables.sql
@@ -10,6 +10,10 @@ PRINT('Medical Necessity');
 DELETE FROM medical_necessity;
 DBCC CHECKIDENT ('medical_necessity', RESEED, 0);
 
+PRINT('Patient Status');
+DELETE FROM patient_status;
+DBCC CHECKIDENT ('patient_status', RESEED, 0);
+
 -- Patient records are deleted last due to foreign key constraints.
 PRINT('Patient');
 DELETE FROM patient;
