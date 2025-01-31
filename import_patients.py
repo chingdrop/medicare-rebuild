08047aec462e2d7ddd93314fa3547f72de85f2d3
Craig Hurley <churley@metflservices.com>
2025-01-31 14:35:38 -0500

    create larger standardize func for patient data

--- import_patients.py
+++ import_patients.py
@@ -3,9 +3,7 @@ import pandas as pd
 from dotenv import load_dotenv
 from pathlib import Path
 
-from standardize_funcs import standardize_insurance_id, fill_primary_payer, fill_primary_payer_id, \
-    standardize_dx_code, standardize_insurance_name, standardize_name, standardize_state, \
-    standardize_email, standardize_mbi
+from standardize_funcs import standardize_patient_data
 from sql_connect import create_alchemy_engine
 
 load_dotenv()
@@ -28,38 +26,7 @@ export_df = pd.read_csv(
     parse_dates=['DOB', 'On-board Date']
 )
 
-export_df['First Name'] = export_df['First Name'].apply(standardize_name, args=(r'[^a-zA-Z\s.-]',))
-export_df['Last Name'] = export_df['Last Name'].apply(standardize_name, args=(r'[^a-zA-Z\s.-]',))
-export_df['Full Name'] = export_df['First Name'] + ' ' + export_df['Last Name']
-export_df['Middle Name'] = export_df['Middle Name'].apply(standardize_name, args=(r'[^a-zA-Z-\s]',))
-export_df['Nickname'] = export_df['Nickname'].str.strip().str.title()
-export_df['Phone Number'] = export_df['Phone Number'].astype(str).str.replace(r'\D', '', regex=True)
-export_df['Gender'] = export_df['Gender'].replace({'Male': 'M', 'Female': 'F'})
-export_df['Email'] = export_df['Email'].apply(standardize_email)
-export_df['Suffix'] = export_df['Suffix'].str.strip().str.title()
-export_df['Social Security'] = export_df['Social Security'].astype(str).str.replace(r'\D', '', regex=True)
-
-# The logic in standardize name can be used for address text as well.
-export_df['Mailing Address'] = export_df['Mailing Address'].apply(standardize_name, args=(r'[^a-zA-Z0-9\s#.-/]',))
-export_df['City'] = export_df['City'].apply(standardize_name, args=(r'[^a-zA-Z-]',))
-export_df['State'] = export_df['State'].apply(standardize_state)
-export_df['Zip code'] = export_df['Zip code'].astype(str).str.split('-', n=1).str[0]
-
-export_df['Medicare ID number'] = export_df['Medicare ID number'].apply(standardize_mbi)
-export_df['DX_Code'] = export_df['DX_Code'].apply(standardize_dx_code)
-export_df['Insurance ID:'] = export_df['Insurance ID:'].apply(standardize_insurance_id)
-export_df['InsuranceID2'] = export_df['InsuranceID2'].apply(standardize_insurance_id)
-export_df['Insurance Name:'] = export_df.apply(fill_primary_payer, axis=1)
-export_df['Insurance ID:'] = export_df.apply(fill_primary_payer_id, axis=1)
-export_df['Insurance Name:'] = export_df['Insurance Name:'].apply(standardize_insurance_name)
-export_df['InsuranceName2'] = export_df['InsuranceName2'].apply(standardize_insurance_name)
-
-previous_patient_statuses = {
-    'DO NOT CALL': 'Do Not Call' ,
-    'In-Active': 'Inactive',
-    'On-Board': 'Onboard'
-}
-export_df['Member_Status'] = export_df['Member_Status'].replace(previous_patient_statuses)
+export_df = standardize_patient_data(export_df)
 
 # Check for database constraints and replace with Null if failed condition.
 failed_df = export_df[export_df['Phone Number'].apply(lambda x: len(str(x)) != 10)]
@@ -72,37 +39,8 @@ failed_df = export_df[export_df['Medicare ID number'].apply(lambda x: len(str(x)
 export_df = export_df[export_df['Medicare ID number'].apply(lambda x: len(str(x)) == 11)]
 # failed_df = export_df[export_df[['First Name', 'Last Name', 'ID', 'DOB', 'Phone Number', 'Gender', 'Mailing Address', 'City', 'State', 'Zip code', 'DX_Code']].isnull().any(axis=1)]
 # failed_df = export_df[export_df[['Insurance ID:', 'Insurance Name:', 'Medicare ID number']].isnull().all(axis=1)]
-
 failed_df.to_csv(data_dir / 'failed_patient_export.csv', index=False)
 
-export_df = export_df.rename(
-    columns={
-        'First Name': 'first_name',
-        'Last Name': 'last_name',
-        'Middle Name': 'middle_name',
-        'Suffix': 'name_suffix',
-        'Full Name': 'full_name',
-        'Nickname': 'nick_name',
-        'DOB': 'date_of_birth',
-        'Gender': 'sex',
-        'Email': 'email', 
-        'Phone Number': 'phone_number',
-        'Social Security': 'social_security',
-        'ID': 'sharepoint_id',
-        'Mailing Address': 'street_address',
-        'City': 'city',
-        'State': 'temp_state',
-        'Zip code': 'zipcode',
-        'Medicare ID number': 'medicare_beneficiary_id',
-        'Insurance ID:': 'primary_payer_id',
-        'Insurance Name:': 'primary_payer_name',
-        'InsuranceID2': 'secondary_payer_id',
-        'InsuranceName2': 'secondary_payer_name',
-        'On-board Date': 'evaluation_datetime',
-        'DX_Code': 'temp_dx_code',
-        'Member_Status': 'temp_status_type'
-    }
-)
 patient_df = export_df[[
     'first_name',
     'last_name',
