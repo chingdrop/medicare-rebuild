dcca7d48627ebd47c9b17c66a6666d9d52f9007a
Craig Hurley <churley@metflservices.com>
2025-01-31 11:51:19 -0500

    move other funcs

--- import_patients.py
+++ import_patients.py
@@ -1,11 +1,9 @@
 import os
-import re
 import pandas as pd
 from dotenv import load_dotenv
 from pathlib import Path
 
-from apply_funcs import standardize_name
-from enums import state_abbreviations, insurance_keywords
+from apply_funcs import extract_insurance_id, fill_primary_payer, fill_primary_payer_id, standardize_dx_code, standardize_insurance_name, standardize_name, standardize_state
 from sql_connect import create_alchemy_engine
 
 load_dotenv()
@@ -27,47 +25,6 @@ export_df = pd.read_csv(
     },
     parse_dates=['DOB', 'On-board Date']
 )
-
-def standardize_state(state):
-    state = str(state).strip().title()
-    return state_abbreviations.get(state, state).upper()
-
-def standardize_dx_code(dx_code):
-    dx_code = str(dx_code).strip()
-    matches = re.finditer(r'[E|I|R]\d+(\.\d+)?', dx_code)
-    matches = [match.group(0).replace('.', '') for match in matches]
-    return ','.join(matches)
-
-def standardize_insurance_name(name):
-    for standard_name, keyword_sets in insurance_keywords.items():
-        for keyword_set in keyword_sets:
-            if all(re.search(r'\b' + re.escape(keyword) + r'\b', str(name).lower()) for keyword in keyword_set):
-                return standard_name
-    return name
-
-def extract_insurance_id(row):
-    insurance_name = str(row['Insurance Name:'])
-    insurance_id = str(row['Insurance ID:'])
-    if not insurance_name == 'Medicare Part B':
-        insurance_id = re.sub(r'[^A-Za-z0-9]', '', insurance_id)
-        id_pattern = r'([A-Za-z]*\d+[A-Za-z]*\d+[A-Za-z]*\d+[A-Za-z]*\d*)'
-        id_in_name = re.search(id_pattern, insurance_name)
-        id_in_id = re.search(id_pattern, insurance_id)
-        if id_in_name:
-            return id_in_name.group(0)
-        elif id_in_id:
-            return id_in_id.group(0)
-    return row['Insurance ID:']
-
-def fill_primary_payer(row):
-    if pd.isnull(row['Insurance Name:']) and pd.isnull(row['Insurance ID:']) and not pd.isnull(row['Medicare ID number']):
-        return 'Medicare Part B'
-    return row['Insurance Name:']
-
-def fill_primary_payer_id(row):
-    if row['Insurance Name:'] == 'Medicare Part B' and pd.isnull(row['Insurance ID:']) :
-        return row['Medicare ID number']
-    return row['Insurance ID:']
 
 export_df['First Name'] = export_df['First Name'].apply(standardize_name, args=(r'[^a-zA-Z\s.-]',))
 export_df['Last Name'] = export_df['Last Name'].apply(standardize_name, args=(r'[^a-zA-Z\s.-]',))
