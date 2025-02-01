d26f0669f6c932d1fdd9ff883d411ebb6029a670
Craig Hurley <chingdorp@pm.me>
2025-02-01 02:25:32 -0500

    use new data import funcs

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 from pathlib import Path
 
-from import_to_sql import DataImporter
+from import_to_sql import import_patient_data, import_device_data, import_patient_note_data, \
+    import_patient_reading_data
 
 
-di = DataImporter()
-di.connect_gps_db()
-di.connect_notes_db()
-di.connect_time_db()
-di.connect_fulfillment_db()
-di.connect_readings_db()
-
-di.import_patient_data(Path.cwd() / 'data' / 'Patient_Export.csv')
-di.import_device_data()
-di.import_patient_note_data()
-di.import_patient_reading_data()
+import_patient_data(Path.cwd() / 'data' / 'Patient_Export.csv')
+import_device_data()
+import_patient_note_data()
+import_patient_reading_data()
