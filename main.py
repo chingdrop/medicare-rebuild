bc34c949ba35830f64a1be1462ddcef70f16c14a
Craig Hurley <churley@metflservices.com>
2025-01-31 20:52:50 -0500

    create main file

---
+++ main.py
@@ -0,0 +1,150 @@
+import os
+import pandas as pd
+from dotenv import load_dotenv
+from pathlib import Path
+
+from helpers import read_sql_file
+from dataframe_utils import add_id_col, standardize_patients, standardize_patient_notes, \
+    standardize_devices, standardize_bp_readings, standardize_bg_readings
+from db_utils import DatabaseManager
+
+load_dotenv()
+gps_db = DatabaseManager(
+    username=os.getenv('LCH_SQL_GPS_USERNAME'),
+    password=os.getenv('LCH_SQL_GPS_PASSWORD'),
+    host=os.getenv('LCH_SQL_GPS_HOST'),
+    database=os.getenv('LCH_SQL_GPS_DB')
+)
+gps_db.connect()
+notes_db = DatabaseManager(
+    username=os.getenv('LCH_SQL_USERNAME'),
+    password=os.getenv('LCH_SQL_PASSWORD'),
+    host=os.getenv('LCH_SQL_HOST'),
+    database=os.getenv('LCH_SQL_SP_NOTES')
+)
+notes_db.connect()
+time_db = DatabaseManager(
+    username=os.getenv('LCH_SQL_USERNAME'),
+    password=os.getenv('LCH_SQL_PASSWORD'),
+    host=os.getenv('LCH_SQL_HOST'),
+    database=os.getenv('LCH_SQL_SP_TIME')
+)
+time_db.connect()
+fulfillment_db = DatabaseManager(
+    username=os.getenv('LCH_SQL_USERNAME'),
+    password=os.getenv('LCH_SQL_PASSWORD'),
+    host=os.getenv('LCH_SQL_HOST'),
+    database=os.getenv('LCH_SQL_SP_FULFILLMENT')
+)
+fulfillment_db
+readings_db = DatabaseManager(
+    username=os.getenv('LCH_SQL_USERNAME'),
+    password=os.getenv('LCH_SQL_PASSWORD'),
+    host=os.getenv('LCH_SQL_HOST'),
+    database=os.getenv('LCH_SQL_SP_READINGS')
+)
+readings_db.connect()
+
+cwd_dir = Path.cwd()
+data_dir = cwd_dir / 'data'
+# Patient data MUST be exported from SharePoint first.
+export_df = pd.read_csv(
+    data_dir / 'Patient_Export.csv',
+    dtype={
+        'Phone Number': 'str',
+        'Social Security': 'str',
+        'Zip code': 'str'
+    },
+    parse_dates=['DOB', 'On-board Date']
+)
+
+get_queries_dir = cwd_dir / 'queries' / 'gets'
+patient_id_stmt = read_sql_file(get_queries_dir / 'get_patient_id.sql')
+notes_stmt = read_sql_file(get_queries_dir / 'get_notes_log.sql')
+time_stmt = read_sql_file(get_queries_dir / 'get_time_log.sql')
+device_stmt = read_sql_file(get_queries_dir / 'get_fulfillment.sql')
+vendor_id_stmt = read_sql_file(get_queries_dir / 'get_vendor_id.sql')
+bp_readings_stmt = read_sql_file(get_queries_dir / 'get_bp_readings.sql')
+bg_readings_stmt = read_sql_file(get_queries_dir / 'get_bg_readings.sql')
+device_id_stmt = read_sql_file(get_queries_dir / 'get_device_id.sql')
+
+notes_df = notes_db.read_sql(notes_stmt, parse_dates=['TimeStamp'])
+time_df = time_db.read_sql(time_stmt, parse_dates=['Start_Time', 'End_Time'])
+device_df = fulfillment_db.read_sql(device_stmt)
+bp_readings_df = readings_db.read_sql(bp_readings_stmt, parse_dates=['Time_Recorded', 'Time_Recieved'])
+bg_readings_df = readings_db.read_sql(bg_readings_stmt, parse_dates=['Time_Recorded', 'Time_Recieved'])
+
+time_df = time_df.rename(
+    columns={
+        'SharPoint_ID': 'SharePoint_ID',
+        'Notes': 'Note_Type'
+    }
+)
+# Left join is needed for patient notes without any call time associated.
+patient_note_df = pd.merge(notes_df, time_df, on=['Note_ID', 'SharePoint_ID', 'LCH_UPN'], how='left')
+patient_note_df['Time_Note'] = patient_note_df['Time_Note'].fillna(patient_note_df['Note_Type'])
+patient_note_df.drop(columns=['Note_ID', 'Note_Type'], inplace=True)
+
+export_df = standardize_patients(export_df)
+patient_note_df = standardize_patient_notes(patient_note_df)
+device_df = standardize_devices(device_df)
+bp_readings_df = standardize_bp_readings(bp_readings_df)
+bg_readings_df = standardize_bg_readings(bg_readings_df)
+
+patient_df = export_df[[
+    'first_name',
+    'last_name',
+    'middle_name',
+    'name_suffix',
+    'full_name',
+    'nick_name',
+    'date_of_birth',
+    'sex',
+    'email',
+    'phone_number',
+    'social_security',
+    'sharepoint_id'
+]]
+address_df = export_df[['street_address', 'city', 'temp_state', 'zipcode', 'sharepoint_id']]
+insurance_df = export_df[[
+    'medicare_beneficiary_id',
+    'primary_payer_id',
+    'primary_payer_name',
+    'secondary_payer_id',
+    'secondary_payer_name',
+    'sharepoint_id'
+]]
+med_nec_df = export_df[['evaluation_datetime', 'temp_dx_code', 'sharepoint_id']]
+
+med_nec_df.loc[:, 'temp_dx_code'] = med_nec_df['temp_dx_code'].str.split(',')
+med_nec_df = med_nec_df.explode('temp_dx_code', ignore_index=True)
+patient_status_df = export_df[['temp_status_type', 'sharepoint_id']]
+patient_status_df['modified_date'] = pd.Timestamp.now()
+patient_status_df['temp_user'] = 'ITHelp'
+
+gps_db.to_sql(patient_df, 'patient', if_exists='append')
+patient_id_df = gps_db.read_sql(patient_id_stmt)
+vendor_id_df = gps_db.read_sql(vendor_id_stmt)
+device_id_df = gps_db.read_sql(device_id_stmt)
+
+address_df = add_id_col(df=address_df, id_df=patient_id_df, col='sharepoint_id')
+insurance_df = add_id_col(df=insurance_df, id_df=patient_id_df, col='sharepoint_id')
+med_nec_df = add_id_col(df=med_nec_df, id_df=patient_id_df, col='sharepoint_id')
+patient_status_df = add_id_col(df=patient_status_df, id_df=patient_id_df, col='sharepoint_id')
+patient_note_df = add_id_col(df=patient_note_df, id_df=patient_id_df, col='sharepoint_id')
+device_df = add_id_col(df=device_df, id_df=patient_id_df, col='sharepoint_id')
+vendor_id_df = vendor_id_df.rename(columns={'name': 'Vendor'})
+device_df = add_id_col(df=device_df, id_df=vendor_id_df, col='Vendor')
+bp_readings_df = add_id_col(df=bp_readings_df, id_df=patient_id_df, col='sharepoint_id')
+bp_readings_df = add_id_col(df=bp_readings_df, id_df=device_id_df, col='patient_id')
+bg_readings_df = add_id_col(df=bg_readings_df, id_df=patient_id_df, col='sharepoint_id')
+bg_readings_df = add_id_col(df=bg_readings_df, id_df=device_id_df, col='patient_id')
+
+gps_db.to_sql(address_df, 'patient_address', if_exists='append')
+gps_db.to_sql(insurance_df, 'patient_insurance', if_exists='append')
+gps_db.to_sql(med_nec_df, 'medical_necessity', if_exists='append')
+gps_db.to_sql(patient_status_df, 'patient_status', if_exists='append')
+gps_db.to_sql(patient_note_df, 'patient_note', if_exists='append')
+gps_db.to_sql(device_df, 'device', if_exists='append')
+gps_db.to_sql(bp_readings_df, 'blood_pressure_reading', if_exists='append')
+gps_db.to_sql(bg_readings_df, 'glucose_reading', if_exists='append')
