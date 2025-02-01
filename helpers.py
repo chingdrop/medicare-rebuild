c205fbf08dba74190bc523f18bd7570fe674046e
Craig Hurley <chingdorp@pm.me>
2025-02-01 14:57:12 -0500

    fix error with null creds and encoding on statement file

--- helpers.py
+++ helpers.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 
-def read_sql_file(filepath: Path) -> str:
-    with open(filepath, 'r') as f:
+def read_sql_file(filepath: Path, encoding=None) -> str:
+    with open(filepath, 'r', encoding=encoding) as f:
         sql_query = f.read()
     return sql_query
