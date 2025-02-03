b73150e4d8a45305adf843723bc116bbe356ef7e
Craig Hurley <churley@metflservices.com>
2025-02-03 18:36:47 -0500

    overhauled class to handle multiple engines

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ from dotenv import load_dotenv
 from import_to_sql import import_patient_data, import_device_data, import_patient_note_data, \
     import_patient_reading_data
 from db_utils import DatabaseManager
-from helpers import read_sql_file
 from logger import setup_logger
 
 
 warnings.filterwarnings("ignore")
 load_dotenv()
-gps_db = DatabaseManager(
+dbm = DatabaseManager()
+dbm.create_engine(
+    'gps',
     username=os.getenv('LCH_SQL_GPS_USERNAME'),
     password=os.getenv('LCH_SQL_GPS_PASSWORD'),
     host=os.getenv('LCH_SQL_GPS_HOST'),
     database=os.getenv('LCH_SQL_GPS_DB')
 )
-with gps_db.begin() as conn:
+with dbm.begin('gps') as conn:
     conn.execute(text("EXEC reset_all_billing_tables"))
 
 logger = setup_logger('main', level='debug')
