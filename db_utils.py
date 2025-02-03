4ac423b7ade2ca16baa8b6a9646b61b31aaa2f94
Craig Hurley <chingdorp@pm.me>
2025-02-03 18:36:47 -0500

    overhauled class to handle multiple engines

--- db_utils.py
+++ db_utils.py
@@ -5,8 +5,13 @@ from sqlalchemy.engine import URL
 
 
 class DatabaseManager:
-    def __init__(self, username, password, host, database, logger=None):
-        self.connection_url = URL.create(
+    def __init__(self, logger=None):
+        
+        self.engines = {}
+        self.logger = logger or logging.getLogger(DatabaseManager.__name__)
+
+    def create_engine(self, name: str, username: str, password: str, host: str, database: str):
+        connection_url = URL.create(
             "mssql+pyodbc",
             username=username,
             password=password,
@@ -18,46 +23,44 @@ class DatabaseManager:
                 "TrustServerCertificate": "yes",
             },
         )
-        self.engine = None
-        self.connection = None
-        self.logger = logger or logging.getLogger(DatabaseManager.__name__)
+        engine = create_engine(connection_url)
+        event.listen(engine, 'before_cursor_execute', self.__receive_before_cursor_execute)
+        self.engines[name] = engine
 
-    def create_engine(self):
-        self.engine = create_engine(self.connection_url)
-        event.listen(self.engine, 'before_cursor_execute', self.__receive_before_cursor_execute)
+    def get_engine(self, name: str):
+        return self.engines[name]
 
-    def begin(self,):
-        if not self.engine:
-            self.create_engine()
-        return self.engine.begin()
+    def begin(self, name: str):
+        return self.engines[name].begin()
 
-    def connect(self,):
-        if not self.engine:
-            self.create_engine()
-        return self.engine.connect()
+    def connect(self, name: str):
+        return self.engines[name].connect()
 
-    def read_sql(self, query: str, parse_dates=None) -> pd.DataFrame:
+    def read_sql(self, query: str, eng_name: str, parse_dates=None) -> pd.DataFrame:
         """Executes a SQL query and returns the result as a DataFrame.
         
         Args:
             - query_string (str): The SQL query to execute.
+            - eng_name (str): Name of the SQLAlchemy engine obj.
             - parse_dates (list or dict, optional): List of column names to parse as datetime or 
             a dictionary specifying column names and their respective date formats.
         
         Returns:
             - pandas.DataFrame: The query results as a DataFrame.
         """
-        df = pd.read_sql(query, self.engine, parse_dates=parse_dates)
+        engine = self.get_engine(eng_name)
+        df = pd.read_sql(query, engine, parse_dates=parse_dates)
         self.logger.debug(f'Query: {query.replace('\n', ' ')}')
         self.logger.debug(f'Reading (rows: {df.shape[0]}, cols: {df.shape[1]})...')
         return df
     
-    def to_sql(self, df: pd.DataFrame, table_name: str, if_exists='fail', index=False) -> None:
+    def to_sql(self, df: pd.DataFrame, table_name: str, eng_name: str, if_exists='fail', index=False) -> None:
         """Save a Pandas DataFrame to a SQL table.
 
         Args:
             - df (pandas.DataFrame): The DataFrame to be written to the SQL table.
             table_name (string): The name of the target SQL table where the DataFrame will be saved.
+            - eng_name (str): Name of the SQLAlchemy engine obj.
             - if_exists (string, optional): Specifies what to do if the table already exists. 
             Options are:
                 - 'fail' (default): Raise a ValueError.
@@ -69,12 +72,14 @@ class DatabaseManager:
         Returns:
             - None: This method performs the database operation and does not return anything.
         """
+        engine = self.get_engine(eng_name)
         self.logger.debug(f'Writing (rows: {df.shape[0]}, cols: {df.shape[1]}) to {table_name}...')
-        df.to_sql(table_name, self.engine, if_exists=if_exists, index=index)
+        df.to_sql(table_name, engine, if_exists=if_exists, index=index)
 
     def dispose(self,):
-        self.engine.dispose()
-        self.engine = None
+        for eng_name, engine in self.engines.items():
+            engine.dispose()
+            del self.engines[eng_name]
 
     @staticmethod
     def __receive_before_cursor_execute(conn, cursor, statement, params, context, executemany):
