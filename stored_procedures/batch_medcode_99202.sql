6e74dce9665cb61974e7795327850c900f643541
Craig Hurley <churley@metflservices.com>
2025-02-05 13:00:40 -0500

    standardize tab format and add today date param

--- stored_procedures/batch_medcode_99202.sql
+++ stored_procedures/batch_medcode_99202.sql
@@ -4,7 +4,6 @@
 -- Description:	Adds 99202 code to medical code for patients.
 -- =============================================
 CREATE PROCEDURE [dbo].[batch_medcode_99202]
-
 AS
 BEGIN
 
@@ -17,34 +16,34 @@ BEGIN
 	-- Where a patient_id in the medical code table with a 9920X code doesn't exist.
 	-- Group by patient_id, only include call time minutes 15 or above and less than 30.
 	SELECT pn.patient_id,
-		MAX(pn.note_datetime) AS last_note
+	MAX(pn.note_datetime) AS last_note
 	INTO #99202
 	FROM patient_note pn
 	JOIN note_type nt
-		ON pn.note_type_id = nt.note_type_id
-		AND nt.name = 'Initial Evaluation'
+	ON pn.note_type_id = nt.note_type_id
+	AND nt.name = 'Initial Evaluation'
 	WHERE NOT EXISTS (
 		SELECT 1
 		FROM medical_code mc
 		JOIN medical_code_type mct 
-			ON mc.med_code_type_id = mct.med_code_type_id
-			AND mct.name IN ('99202', '99203', '99204', '99205')
+		ON mc.med_code_type_id = mct.med_code_type_id
+		AND mct.name IN ('99202', '99203', '99204', '99205')
 		WHERE mc.patient_id = pn.patient_id
 	)
 	GROUP BY pn.patient_id
 	HAVING FLOOR(SUM(pn.call_time_seconds)) / 60 >= 15
-		AND FLOOR(SUM(pn.call_time_seconds)) / 60 < 30;
+	AND FLOOR(SUM(pn.call_time_seconds)) / 60 < 30;
 
 	-- Using the #99202 temporary table.
 	-- Insert patient_id, medical_code_type and latest note datetime into medical code table.
 	INSERT INTO medical_code (patient_id, med_code_type_id, timestamp_applied)
 	SELECT t.patient_id,
-		(
+	(
 		SELECT mct.med_code_type_id
 		FROM medical_code_type mct
 		WHERE mct.name = '99202'
-		),
-		t.last_note
+	),
+	t.last_note
 	FROM #99202 t;
 
 END
--- stored_procedures/batch_medcode_99453.sql
+++ stored_procedures/batch_medcode_99453.sql
@@ -5,7 +5,6 @@
 -- Description:	Adds 99453 code to medical code for patients with 16 distinct days of testing.
 -- =============================================
 CREATE PROCEDURE [dbo].[batch_medcode_99453]
-
 AS
 BEGIN
 
@@ -23,44 +22,43 @@ BEGIN
 	-- Group by patient_id and count the distinct dates of eith blood pressure or glucose recevied readings.
 	SELECT d.patient_id,
 		CASE
-			WHEN MAX(gr.received_datetime) > MAX(bpr.received_datetime)
-			THEN MAX(gr.received_datetime)
-			ELSE MAX(bpr.received_datetime)
+		WHEN MAX(gr.received_datetime) > MAX(bpr.received_datetime)
+		THEN MAX(gr.received_datetime)
+		ELSE MAX(bpr.received_datetime)
 		END AS last_reading
 	INTO #99453
 	FROM device d
 	LEFT JOIN glucose_reading gr
-		ON d.device_id = gr.device_id
+	ON d.device_id = gr.device_id
 	LEFT JOIN blood_pressure_reading bpr
-		ON d.device_id = bpr.device_id
+	ON d.device_id = bpr.device_id
 	WHERE NOT EXISTS (
 		SELECT 1
 		FROM medical_code mc
 		JOIN medical_code_type mct 
-			ON mc.med_code_type_id = mct.med_code_type_id
-			AND mct.name = '99453'
+		ON mc.med_code_type_id = mct.med_code_type_id
+		AND mct.name = '99453'
 		JOIN medical_code_device mcd
-			ON mc.med_code_id = mcd.med_code_id
+		ON mc.med_code_id = mcd.med_code_id
 		WHERE mc.patient_id = d.patient_id
-			AND mcd.device_id = d.device_id
+		AND mcd.device_id = d.device_id
 	)
 	GROUP BY d.patient_id
 	HAVING COUNT(DISTINCT CAST(gr.received_datetime AS DATE)) >= 16
-		OR COUNT(DISTINCT CAST(bpr.received_datetime AS DATE)) >= 16;
+	OR COUNT(DISTINCT CAST(bpr.received_datetime AS DATE)) >= 16;
 
 	-- Using the #99453 temporary table.
 	-- Insert patient_id, medical_code_type and latest reading datetime into medical code table.
 	-- Return the med_code_id and patient_id.
 	INSERT INTO medical_code (patient_id, med_code_type_id, timestamp_applied)
-	OUTPUT INSERTED.med_code_id, INSERTED.patient_id 
-		INTO @inserted_codes (med_code_id, patient_id)
+	OUTPUT INSERTED.med_code_id, INSERTED.patient_id INTO @inserted_codes (med_code_id, patient_id)
 	SELECT t.patient_id,
-		(
+	(
 		SELECT mct.med_code_type_id
 		FROM medical_code_type mct
 		WHERE mct.name = '99453'
-		),
-		t.last_reading
+	),
+	t.last_reading
 	FROM #99453 t;
 	
 	-- Insert the variable @inserted_codes into medical code device table.
@@ -68,6 +66,6 @@ BEGIN
 	SELECT ic.med_code_id, d.device_id
 	FROM @inserted_codes ic
 	JOIN device d
-		ON ic.patient_id = d.patient_id;
+	ON ic.patient_id = d.patient_id;
 
 END
