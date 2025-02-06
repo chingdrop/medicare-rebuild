fedba15ec9ae51fbf512acfaec0c0089216ac0f5
Craig Hurley <churley@metflservices.com>
2025-02-06 12:44:37 -0500

    move reset medical code tables to a sproc

--- queries/resets/reset_medical_code_tables.sql
+++
@@ -1,8 +0,0 @@
--- Medical Code Device records are deleted sooner due to foreign key constraints.
-PRINT('Medical Code Device');
-DELETE FROM medical_code_device;
-DBCC CHECKIDENT ('medical_code_device', RESEED, 0);
-
-PRINT('Medical Code');
-DELETE FROM medical_code;
-DBCC CHECKIDENT ('medical_code', RESEED, 0);
