0f8a6cdfc6a22fa647f63de05c14969b72ffea97
Craig Hurley <churley@metflservices.com>
2025-02-06 11:54:57 -0500

    separate 99453 and 99454 for bp and bg readings

---
+++ queries/tests/test_medcode_99454_bp.sql
@@ -0,0 +1,8 @@
+SELECT d.patient_id,
+	MAX(bpr.received_datetime) latest_reading
+FROM device d
+JOIN blood_pressure_reading bpr
+ON d.device_id = bpr.device_id
+AND bpr.received_datetime >= DATEADD(day, -30, GETDATE())
+GROUP BY d.patient_id
+HAVING COUNT(DISTINCT CAST(bpr.received_datetime AS DATE)) >= 16;
--- stored_procedures/batch_medcode_99453_bp.sql
+++ stored_procedures/batch_medcode_99453_bp.sql
@@ -2,35 +2,29 @@
 -- Author:		Craig Hurley
 -- Create date: 12/20/24
 -- Revision date: 01/27/25
--- Description:	Adds 99453 code to medical code for patients with 16 distinct days of testing.
+-- Description:	Adds 99453 to medical code for patients with 16 distinct days of blood pressure testing.
 -- =============================================
-CREATE PROCEDURE [dbo].[batch_medcode_99453]
+CREATE PROCEDURE [dbo].[batch_medcode_99453_bp]
 AS
 BEGIN
 
 	SET NOCOUNT ON;
-	DROP TABLE IF EXISTS #99453;
+	DROP TABLE IF EXISTS #99453_bp;
 
 	DECLARE @inserted_codes TABLE (
 		med_code_id INT, patient_id INT
 	);
 
-	-- Create a temporary table #99453.
-	-- Select patient_id and the latest reading blood pressure or glucose reading date.
-	-- Selecting from the patients, devices. Left join on both readings tables, this ensures you get all patients.
+	-- Create a temporary table #99453_bp.
+	-- Select patient_id and the latest reading blood pressure received reading date.
+	-- Selecting from the patients, devices and join on blood pressure readings table.
 	-- Where a patient_id and device_id in the medical code table with a 99453 code doesn't exist.
-	-- Group by patient_id and count the distinct dates of eith blood pressure or glucose recevied readings.
+	-- Group by patient_id and count the distinct dates of blood pressure recevied readings.
 	SELECT d.patient_id,
-		CASE
-		WHEN MAX(gr.received_datetime) > MAX(bpr.received_datetime)
-		THEN MAX(gr.received_datetime)
-		ELSE MAX(bpr.received_datetime)
-		END AS last_reading
-	INTO #99453
+		MAX(bpr.received_datetime) latest_reading
+	INTO #99453_bp
 	FROM device d
-	LEFT JOIN glucose_reading gr
-	ON d.device_id = gr.device_id
-	LEFT JOIN blood_pressure_reading bpr
+	JOIN blood_pressure_reading bpr
 	ON d.device_id = bpr.device_id
 	WHERE NOT EXISTS (
 		SELECT 1
@@ -44,8 +38,7 @@ BEGIN
 		AND mcd.device_id = d.device_id
 	)
 	GROUP BY d.patient_id
-	HAVING COUNT(DISTINCT CAST(gr.received_datetime AS DATE)) >= 16
-	OR COUNT(DISTINCT CAST(bpr.received_datetime AS DATE)) >= 16;
+	HAVING COUNT(DISTINCT CAST(bpr.received_datetime AS DATE)) >= 16;
 
 	-- Using the #99453 temporary table.
 	-- Insert patient_id, medical_code_type and latest reading datetime into medical code table.
@@ -58,8 +51,8 @@ BEGIN
 		FROM medical_code_type mct
 		WHERE mct.name = '99453'
 	),
-	t.last_reading
-	FROM #99453 t;
+	t.latest_reading
+	FROM #99453_bp t;
 	
 	-- Insert the variable @inserted_codes into medical code device table.
 	INSERT INTO medical_code_device (med_code_id, device_id)
