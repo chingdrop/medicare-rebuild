5fa642f71b33646cd8c8c64339288b5141897176
Craig Hurley <churley@metflservices.com>
2025-02-13 15:44:20 -0500

    separate query into CTEs for grouping anomalies

--- stored_procedures/create_billing_report.sql
+++ stored_procedures/create_billing_report.sql
@@ -6,47 +6,61 @@ BEGIN
 
 	SET NOCOUNT ON;
 
-	SELECT MAX(mc.timestamp_applied) AS date_of_service,
-		p.patient_id,
-		p.sharepoint_id,
-		p.first_name,
-		p.last_name,
-		p.name_suffix,
-		p.date_of_birth,
-		p.sex,
-		p.phone_number,
-		pa.street_address,
-		pa.city,
-		pa.temp_state,
-		pa.zipcode,
-		pin.medicare_beneficiary_id,
-		pin.primary_payer_name,
-		pin.primary_payer_id,
-		pin.secondary_payer_name,
-		pin.secondary_payer_id,
-		STRING_AGG(temp_dx_code, ',') AS dx_codes,
-		COUNT(CASE WHEN mct.name = '99202' THEN 1 END) AS count_99202,
-		COUNT(CASE WHEN mct.name = '99453' THEN 1 END) AS count_99453,
-		COUNT(CASE WHEN mct.name = '99454' THEN 1 END) AS count_99454,
-		COUNT(CASE WHEN mct.name = '99457' THEN 1 END) AS count_99457,
-		COUNT(CASE WHEN mct.name = '99458' THEN 1 END) AS count_99458
+	WITH daily_patient_interactions AS (
+		SELECT mc.patient_id,
+			CAST(mc.timestamp_applied AS DATE) AS date_of_service,
+			SUM(CASE WHEN mct.name = '99202' THEN 1 ELSE 0 END) AS count_99202,
+			SUM(CASE WHEN mct.name = '99453' THEN 1 ELSE 0 END) AS count_99453,
+			SUM(CASE WHEN mct.name = '99454' THEN 1 ELSE 0 END) AS count_99454,
+			SUM(CASE WHEN mct.name = '99457' THEN 1 ELSE 0 END) AS count_99457,
+			SUM(CASE WHEN mct.name = '99458' THEN 1 ELSE 0 END) AS count_99458
+		FROM medical_code mc
+		JOIN medical_code_type mct
+		ON mc.med_code_type_id = mct.med_code_type_id
+		WHERE mc.timestamp_applied >= @start_date
+		AND mc.timestamp_applied <= @end_date
+		GROUP BY CAST(mc.timestamp_applied AS DATE), mc.patient_id
+	),
+	patient_dx_codes AS (
+		SELECT patient_id,
+			STRING_AGG(temp_dx_code, ', ') AS dx_codes
+		FROM medical_necessity
+		GROUP BY patient_id
+	)
+
+	SELECT dpi.date_of_service AS DateOfService,
+		p.sharepoint_id AS ID,
+		p.first_name AS FirstName,
+		p.middle_name AS MiddleName,
+		p.last_name AS LastName,
+		p.name_suffix AS Suffix,
+		p.date_of_birth AS DOB,
+		p.phone_number AS PhoneNumber,
+		pa.street_address AS Address,
+		pa.city AS City,
+		pa.temp_state AS State,
+		pa.zipcode AS Zipcode,
+		p.sex AS Gender,
+		pin.medicare_beneficiary_id AS MedicareNumber,
+		pin.primary_payer_name AS PrimaryPayer,
+		pin.primary_payer_id AS PrimaryPayerID,
+		pin.secondary_payer_name AS SecondaryPayer,
+		pin.secondary_payer_id AS SecondaryPayerID,
+		pdc.dx_codes AS DXCodes,
+		dpi.count_99202 AS '99202',
+		dpi.count_99453 AS '99453',
+		dpi.count_99454 AS '99454',
+		dpi.count_99457 AS '99457',
+		dpi.count_99458 AS '99458'
 	FROM patient p
 	JOIN patient_address pa
 	ON p.patient_id = pa.patient_id
 	JOIN patient_insurance pin
 	ON p.patient_id = pin.patient_id
-	JOIN medical_necessity mn
-	ON p.patient_id = mn.patient_id
-	JOIN medical_code mc
-	ON p.patient_id = mc.patient_id
-	AND mc.timestamp_applied <= @end_date
-	AND mc.timestamp_applied >= @start_date
-	JOIN medical_code_type mct
-	ON mc.med_code_type_id = mct.med_code_type_id
-	GROUP BY mc.med_code_id, 
-		p.patient_id, p.sharepoint_id, p.first_name, p.last_name, p.name_suffix, p.date_of_birth, p.sex, p.phone_number, 
-		pa.street_address, pa.city, pa.temp_state, pa.zipcode,
-		pin.medicare_beneficiary_id, pin.primary_payer_name, pin.primary_payer_id, pin.secondary_payer_name, pin.secondary_payer_id
+	JOIN patient_dx_codes pdc
+	ON p.patient_id = pdc.patient_id
+	JOIN daily_patient_interactions dpi
+	ON p.patient_id = dpi.patient_id
 	ORDER BY p.patient_id
 	RETURN;
 END
