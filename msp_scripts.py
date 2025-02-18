fb50f9f0403f685ac7b446d6ed82b4a38884cdb2
Craig Hurley <churley@metflservices.com>
2025-02-18 12:51:05 -0500

    save MS Graph response to csv

--- msp_scripts.py
+++ msp_scripts.py
@@ -1,4 +1,6 @@
 import os
+import pandas as pd
+from pathlib import Path
 
 from api_utils import MSGraphApi
 
@@ -9,4 +11,6 @@ msg = MSGraphApi(
     client_secret=os.getenv('AZURE_CLIENT_SECRET')
 )
 msg.request_access_token()
-print(msg.get_group_members('4bbe3379-1250-4522-92e6-017f77517470'))
+data = msg.get_group_members('4bbe3379-1250-4522-92e6-017f77517470')
+df = pd.DataFrame(data['value'])
+df.to_csv(Path.cwd() / 'data' / 'test_users.csv', index=False)
