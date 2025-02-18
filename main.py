ee6c6995fc2f0b5ee8231f7b3bc32d6ebc670666
Craig Hurley <chingdorp@pm.me>
2025-02-18 14:54:28 -0500

    import user data with other data

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 from import_to_sql import import_patient_data, import_device_data, import_patient_note_data, \
-    import_patient_reading_data
+    import_patient_reading_data, import_user_data
 from db_utils import DatabaseManager
 from logger import setup_logger
 
@@ -22,6 +22,7 @@ gps.execute_query("EXEC reset_all_billing_tables")
 gps.close()
 
 logger = setup_logger('main', level='debug')
+import_user_data(logger=logger)
 import_patient_data(Path.cwd() / 'data' / 'Patient_Export.csv', snapshot=True, logger=logger)
 import_device_data(snapshot=True, logger=logger)
 import_patient_note_data(snapshot=True, logger=logger)
