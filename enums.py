3a3fe13c515890fde8641d9ce2a4a56bd613d928
Craig Hurley <chingdorp@pm.me>
2025-02-24 11:01:40 -0500

    change double quote to single quote

--- enums.py
+++ enums.py
@@ -1,230 +1,230 @@
 state_abbreviations = {
-    "Alabama": "AL",
-    "Alaska": "AK",
-    "Arizona": "AZ",
-    "Arkansas": "AR",
-    "American Samoa": "AS",
-    "California": "CA",
-    "Colorado": "CO",
-    "Connecticut": "CT",
-    "Delaware": "DE",
-    "District of Columbia": "DC",
-    "Florida": "FL",
-    "Georgia": "GA",
-    "Guam": "GU",
-    "Hawaii": "HI",
-    "Idaho": "ID",
-    "Illinois": "IL",
-    "Indiana": "IN",
-    "Iowa": "IA",
-    "Kansas": "KS",
-    "Kentucky": "KY",
-    "Louisiana": "LA",
-    "Maine": "ME",
-    "Maryland": "MD",
-    "Massachusetts": "MA",
-    "Michigan": "MI",
-    "Minnesota": "MN",
-    "Mississippi": "MS",
-    "Missouri": "MO",
-    "Montana": "MT",
-    "Nebraska": "NE",
-    "Nevada": "NV",
-    "New Hampshire": "NH",
-    "New Jersey": "NJ",
-    "New Mexico": "NM",
-    "New York": "NY",
-    "North Carolina": "NC",
-    "North Dakota": "ND",
-    "Northern Mariana Islands": "MP",
-    "Ohio": "OH",
-    "Oklahoma": "OK",
-    "Oregon": "OR",
-    "Pennsylvania": "PA",
-    "Puerto Rico": "PR",
-    "Rhode Island": "RI",
-    "South Carolina": "SC",
-    "South Dakota": "SD",
-    "Tennessee": "TN",
-    "Texas": "TX",
-    "Trust Territories": "TT",
-    "Utah": "UT",
-    "Vermont": "VT",
-    "Virgin Islands": "VI",
-    "Virginia": "VA",
-    "Washington": "WA",
-    "West Virginia": "WV",
-    "Wisconsin": "WI",
-    "Wyoming": "WY"
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'American Samoa': 'AS',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'District of Columbia': 'DC',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Guam': 'GU',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Northern Mariana Islands': 'MP',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Puerto Rico': 'PR',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Trust Territories': 'TT',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virgin Islands': 'VI',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY'
 }
 
 
 # Key word lists must have all elements match in the target string.
 # Spelling errors and abbreviations are added to increase matching.
 insurance_keywords = {
-    "AARP-PPO": [
-        ["aarp"]
+    'AARP-PPO': [
+        ['aarp']
     ],
-    "Aetna Insurance": [
-        ["aetna"]
+    'Aetna Insurance': [
+        ['aetna']
     ],
-    "Ambetter of Arkansas": [
-        ["ambetter", "arkansas"]
+    'Ambetter of Arkansas': [
+        ['ambetter', 'arkansas']
     ],
-    "American Continental Insurance": [
-        ["american", "continental"]
+    'American Continental Insurance': [
+        ['american', 'continental']
     ],
-    "Amerigroup": [
-        ["amerigroup"],
-        ["amerigrp"]
+    'Amerigroup': [
+        ['amerigroup'],
+        ['amerigrp']
     ],
-    "Bankers Life And Casualty": [
-        ["bankers", "life"]
+    'Bankers Life And Casualty': [
+        ['bankers', 'life']
     ],
-    "BCBS Federal": [
-        ["blue", "cross", "federal"],
-        ["blue", "cross", "fed"],
-        ["bcbs", "federal"],
-        ["bcbs", "fed"]
+    'BCBS Federal': [
+        ['blue', 'cross', 'federal'],
+        ['blue', 'cross', 'fed'],
+        ['bcbs', 'federal'],
+        ['bcbs', 'fed']
     ],
-    "Blue Cross and Blue Shield of Florida": [
-        ["blue", "cross"],
-        ["blue", "shield"],
-        ["bluecross"],
-        ["bcbs"],
-        ["anthem"]
+    'Blue Cross and Blue Shield of Florida': [
+        ['blue', 'cross'],
+        ['blue', 'shield'],
+        ['bluecross'],
+        ['bcbs'],
+        ['anthem']
     ],
-    "Care Improvement Plus": [
-        ["care", "improvement"]
+    'Care Improvement Plus': [
+        ['care', 'improvement']
     ],
-    "Care Plus": [
-        ["care", "plus"],
-        ["careplus"]
+    'Care Plus': [
+        ['care', 'plus'],
+        ['careplus']
     ],
-    "Caresource": [
-        ["care", "source"],
-        ["caresource"]
+    'Caresource': [
+        ['care', 'source'],
+        ['caresource']
     ],
-    "Central California Alliance for Health": [
-        ["central", "california"],
-        ["central", "ca"]
+    'Central California Alliance for Health': [
+        ['central', 'california'],
+        ['central', 'ca']
     ],
-    "Champ Va": [
-        ["champ", "va"]
+    'Champ Va': [
+        ['champ', 'va']
     ],
-    "Cigna": [
-        ["cigna"]
+    'Cigna': [
+        ['cigna']
     ],
-    "Colonial Penn Life Insurance CO": [
-        ["colonial", "penn"]
+    'Colonial Penn Life Insurance CO': [
+        ['colonial', 'penn']
     ],
-    "Commonwealth Care Alliance": [
-        ["commonwealth", "care"],
-        ["commonwealth", "health"]
+    'Commonwealth Care Alliance': [
+        ['commonwealth', 'care'],
+        ['commonwealth', 'health']
     ],
-    "Community Health Plan": [
-        ["community", "health"]
+    'Community Health Plan': [
+        ['community', 'health']
     ],
-    "CSI Medicare Supplement": [
-        ["csi", "medicare"]
+    'CSI Medicare Supplement': [
+        ['csi', 'medicare']
     ],
-    "Devoted Health": [
-        ["devoted"]
+    'Devoted Health': [
+        ['devoted']
     ],
-    "Emblem Health": [
-        ["emblem"]
+    'Emblem Health': [
+        ['emblem']
     ],
-    "Fidelis Care New York": [
-        ["fidelis"]
+    'Fidelis Care New York': [
+        ['fidelis']
     ],
-    "Freedom Health": [
-        ["freedom"]
+    'Freedom Health': [
+        ['freedom']
     ],
-    "GEHA": [
-        ["geha"]
+    'GEHA': [
+        ['geha']
     ],
-    "Great southern life insurance": [
-        ["great", "southern"]
+    'Great southern life insurance': [
+        ['great', 'southern']
     ],
-    "Harvard Pilgrim Health Plan": [
-        ["harvard", "pilgrim"]
+    'Harvard Pilgrim Health Plan': [
+        ['harvard', 'pilgrim']
     ],
-    "Healthfirst Health Plan": [
-        ["healthfirst"]
+    'Healthfirst Health Plan': [
+        ['healthfirst']
     ],
-    "Humana": [
-        ["humana"],
-        ["humann"],
-        ["humanna"]
+    'Humana': [
+        ['humana'],
+        ['humann'],
+        ['humanna']
     ],
-    "Inland Empire Health Plan": [
-        ["inland", "empire"]
+    'Inland Empire Health Plan': [
+        ['inland', 'empire']
     ],
-    "Kaiser": [
-        ["kaiser"],
-        ["keiser"]
+    'Kaiser': [
+        ['kaiser'],
+        ['keiser']
     ],
-    "Kern Health Systems": [
-        ["kern", "health"],
-        ["kern", "family"]
+    'Kern Health Systems': [
+        ['kern', 'health'],
+        ['kern', 'family']
     ],
-    "Medicaid of California": [
-        ["medicaid", "california"],
-        ["medicaid", "ca"]
+    'Medicaid of California': [
+        ['medicaid', 'california'],
+        ['medicaid', 'ca']
     ],
-    "Medicaid of Florida": [
-        ["medicaid", "florida"],
-        ["medicaid", "fl"]
+    'Medicaid of Florida': [
+        ['medicaid', 'florida'],
+        ['medicaid', 'fl']
     ],
-    "Medical Mutual of Ohio": [
-        ["medical", "mutual", "ohio"],
-        ["medical", "mutual", "oh"]
+    'Medical Mutual of Ohio': [
+        ['medical', 'mutual', 'ohio'],
+        ['medical', 'mutual', 'oh']
     ],
-    "Meridian Health Plan": [
-        ["meridian", "health"]
+    'Meridian Health Plan': [
+        ['meridian', 'health']
     ],
-    "Molina Healthcare": [
-        ["molina"]
+    'Molina Healthcare': [
+        ['molina']
     ],
-    "Mutual of Omaha": [
-        ["mutual", "omaha"]
+    'Mutual of Omaha': [
+        ['mutual', 'omaha']
     ],
-    "Preferred Care Partners": [
-        ["preferred", "care", "partner"]
+    'Preferred Care Partners': [
+        ['preferred', 'care', 'partner']
     ],
-    "Sunshine state Health Plan": [
-        ["sunshine", "health"],
-        ["sunshine", "insurance"]
+    'Sunshine state Health Plan': [
+        ['sunshine', 'health'],
+        ['sunshine', 'insurance']
     ],
-    "Transamerica Life Insurance Co": [
-        ["trans", "america"]
+    'Transamerica Life Insurance Co': [
+        ['trans', 'america']
     ],
-    "Tricare for Life": [
-        ["tricare"]
+    'Tricare for Life': [
+        ['tricare']
     ],
-    "United American":[
-        ["united", "american"]
+    'United American':[
+        ['united', 'american']
     ],
-    "United HealthCare": [
-        ["united", "health"],
-        ["united", "healthcare"],
-        ["united", "health", "care"],
-        ["uhc"]
+    'United HealthCare': [
+        ['united', 'health'],
+        ['united', 'healthcare'],
+        ['united', 'health', 'care'],
+        ['uhc']
     ],
-    "UPMC Health Plan": [
-        ["upmc"]
+    'UPMC Health Plan': [
+        ['upmc']
     ],
-    "Wellcare": [
-        ["wellcare"]
+    'Wellcare': [
+        ['wellcare']
     ],
-    "Wellpoint": [
-        ["wellpoint"]
+    'Wellpoint': [
+        ['wellpoint']
     ],
-    "Western Health Advantage": [
-        ["western", "health", "advantage"]
+    'Western Health Advantage': [
+        ['western', 'health', 'advantage']
     ],
-    "Western United Life Assurance": [
-        ["western", "united"]
+    'Western United Life Assurance': [
+        ['western', 'united']
     ]
 }
 
