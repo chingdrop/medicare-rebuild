6114246d68bbae3fd488fba19c5678619200e1ed
Craig Hurley <churley@metflservices.com>
2025-03-04 02:19:55 -0500

    import tenovi device and reading data to sql

--- import_to_sql.py
+++ import_to_sql.py
@@ -3,7 +3,7 @@ import pandas as pd
 from datetime import datetime
 from pathlib import Path
 
-from api_utils import MSGraphApi
+from api_utils import MSGraphApi, TenoviApi
 from dataframe_utils import add_id_col, standardize_patients, standardize_patient_notes, \
     standardize_devices, standardize_bp_readings, standardize_bg_readings, \
     patient_check_db_constraints, \
@@ -13,8 +13,7 @@ from db_utils import DatabaseManager
 from helpers import get_last_month_billing_cycle
 from logger import setup_logger
 from queries import get_patient_id_stmt, get_notes_log_stmt, get_time_log_stmt, \
-    get_fulfillment_stmt, get_vendor_id_stmt, get_bg_readings_stmt, get_bp_readings_stmt, \
-    get_device_id_stmt
+    get_vendor_id_stmt, get_device_id_stmt, get_device_name_stmt
 
 
 def import_user_data(logger=setup_logger('import_user_data')):
@@ -145,6 +144,29 @@ def import_patient_note_data(logger=setup_logger('import_patient_notes')):
 
 
 def import_device_data(logger=setup_logger('import_devices')):
+    tenovi = TenoviApi(
+        'livecare',
+        api_key=os.getenv('TENOVI_API_KEY'),
+        logger=logger
+    )
+    res_data = tenovi.get_devices()
+    device_data = []
+    for device in res_data:
+        device_dict = {
+            'hwi_id': device['id'],
+            'name': device['device']['name'],
+            'hardware_uuid': device['device']['hardware_uuid'],
+            'status': device['status'],
+            'connected_datetime': device['connected_on'],
+            'unlinked_datetime': device['unlinked_on'],
+            'last_measurement_datetime': device['last_measurement'],
+            'created_datetime': device['device']['created'],
+            'sharepoint_id': device['patient_id']
+        }
+        device_data.append(device_dict)
+    device_df = pd.DataFrame(device_data)
+    device_df = standardize_devices(device_df)
+    
     gps = DatabaseManager(logger=logger)
     gps.create_engine(
         username=os.getenv('LCH_SQL_GPS_USERNAME'),
@@ -152,23 +174,12 @@ def import_device_data(logger=setup_logger('import_devices')):
         host=os.getenv('LCH_SQL_GPS_HOST'),
         database=os.getenv('LCH_SQL_GPS_DB')
     )
-    fulfillment_db = DatabaseManager(logger=logger)
-    fulfillment_db.create_engine(
-        username=os.getenv('LCH_SQL_USERNAME'),
-        password=os.getenv('LCH_SQL_PASSWORD'),
-        host=os.getenv('LCH_SQL_HOST'),
-        database=os.getenv('LCH_SQL_SP_FULFILLMENT')
-    )
-    
-    device_df = fulfillment_db.read_sql(get_fulfillment_stmt)
-    device_df = standardize_devices(device_df)
-
     patient_id_df = gps.read_sql(get_patient_id_stmt)
     vendor_id_df = gps.read_sql(get_vendor_id_stmt)
 
     device_df = add_id_col(df=device_df, id_df=patient_id_df, col='sharepoint_id')
-    vendor_id_df = vendor_id_df.rename(columns={'name': 'Vendor'})
-    device_df = add_id_col(df=device_df, id_df=vendor_id_df, col='Vendor')
+    vendor_id_df = vendor_id_df.rename(columns={'name': 'vendor'})
+    device_df = add_id_col(df=device_df, id_df=vendor_id_df, col='vendor')
     
     gps.to_sql(device_df, 'device', if_exists='append')
     gps.close()
@@ -182,32 +193,63 @@ def import_patient_reading_data(logger=setup_logger('import_patient_readings')):
         host=os.getenv('LCH_SQL_GPS_HOST'),
         database=os.getenv('LCH_SQL_GPS_DB')
     )
-    readings_db = DatabaseManager(logger=logger)
-    readings_db.create_engine(
-        username=os.getenv('LCH_SQL_USERNAME'),
-        password=os.getenv('LCH_SQL_PASSWORD'),
-        host=os.getenv('LCH_SQL_HOST'),
-        database=os.getenv('LCH_SQL_SP_READINGS')
-    )
-    start_date, _ = get_last_month_billing_cycle()
-    end_date = datetime.now()
-    bp_readings_df = readings_db.read_sql(get_bp_readings_stmt,
-                                  params=(start_date, end_date),
-                                  parse_dates=['Time_Recorded', 'Time_Recieved'])
-    bg_readings_df = readings_db.read_sql(get_bg_readings_stmt,
-                                  params=(start_date, end_date),
-                                  parse_dates=['Time_Recorded', 'Time_Recieved'])
+    device_name_df = gps.read_sql(get_device_name_stmt)
+    gluc_device_ids = device_name_df['hwi_id'].loc[device_name_df['name'].str.contains('Glucometer', na=False)].to_list()
+    bp_device_ids = device_name_df['hwi_id'].loc[device_name_df['name'].str.contains('BPM', na=False)].to_list()
 
+    tenovi = TenoviApi(
+        'livecare',
+        api_key=os.getenv('TENOVI_API_KEY'),
+        logger=logger
+    )
+
+    start_date, _ = get_last_month_billing_cycle()
+    bg_total_readings = []
+    for gluc_device_id in gluc_device_ids:
+        bg_readings_data = tenovi.get_readings(
+            gluc_device_id,
+            metric='glucose',
+            created_gte=start_date
+        )
+        if bg_readings_data:
+            bg_readings_data = [
+                {
+                    'hwi_id': data['hwi_device_id'],
+                    'glucose_reading': data['value_1'],
+                    'recorded_datetime': data['timestamp'],
+                    'received_datetine': data['created']
+                } for data in bg_readings_data
+            ]
+            bg_total_readings.extend(bg_readings_data)
+
+    bp_total_readings = []
+    for bp_device_id in bp_device_ids:
+        bp_readings_data = tenovi.get_readings(
+            bp_device_id,
+            metric='blood_pressure',
+            created_gte=start_date
+        )
+        if bp_readings_data:
+            bp_readings_data = [
+                {
+                    'hwi_id': data['hwi_device_id'],
+                    'systolic_reading': data['value_1'],
+                    'diastolic_reading': data['value_2'],
+                    'recorded_datetime': data['timestamp'],
+                    'received_datetine': data['created']
+                } for data in bp_readings_data
+            ]
+            bp_total_readings.extend(bp_readings_data)
+
+    bg_readings_df = pd.DataFrame(bg_total_readings)
+    bp_readings_df = pd.DataFrame(bp_total_readings)
     bp_readings_df = standardize_bp_readings(bp_readings_df)
     bg_readings_df = standardize_bg_readings(bg_readings_df)
 
-    patient_id_df = gps.read_sql(get_patient_id_stmt)
     device_id_df = gps.read_sql(get_device_id_stmt)
     
-    bp_readings_df = add_id_col(df=bp_readings_df, id_df=patient_id_df, col='sharepoint_id')
-    bp_readings_df = add_id_col(df=bp_readings_df, id_df=device_id_df, col='patient_id')
-    bg_readings_df = add_id_col(df=bg_readings_df, id_df=patient_id_df, col='sharepoint_id')
-    bg_readings_df = add_id_col(df=bg_readings_df, id_df=device_id_df, col='patient_id')
+    bp_readings_df = add_id_col(df=bp_readings_df, id_df=device_id_df, col='hwi_id')
+    bg_readings_df = add_id_col(df=bg_readings_df, id_df=device_id_df, col='hwi_id')
     
     gps.to_sql(bp_readings_df, 'blood_pressure_reading', if_exists='append')
     gps.to_sql(bg_readings_df, 'glucose_reading', if_exists='append')
